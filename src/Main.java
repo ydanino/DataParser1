5dc64383ee41b749ee27e3df678246ded048428f
ydanino <ydanino999@student.fuhsd.org>
2019-03-04 10:12:31 -0800

    add test of file reading to main method

--- src/Main.java
+++ src/Main.java
@@ -5,4 +5,22 @@
 
 
 public class Main {
+
+    /***
+     * weekend hw code
+     *
+     * split by /n
+     * remove all !@#$%^&*(
+     * split by ,
+     *
+     *
+     *
+     */
+
+    public static void main(String[] args) {
+        //test of utils
+
+        String data = Utils.readFileAsString("data/2016_Presidential_Results.csv");
+        System.out.println(data);
+    }
 }
